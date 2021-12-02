919c8d9e0de36ed32269f3343d6051def17e95ca
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-02 08:08:42 -0700

    Day 2 Part 2 complete

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -18,6 +19,30 @@ fn part1() {
             "forward" => forward += number,
             "down" => depth += number,
             "up" => depth -= number,
+            _ => panic!("unknown command"),
+        }
+    }
+
+    println!("{}", forward * depth);
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut forward = 0;
+    let mut depth = 0;
+    let mut aim = 0;
+    for line in file.lines().map(|l| l.unwrap()) {
+        let (command, number) = line.split_once(' ').unwrap();
+        let number: u32 = number.parse().unwrap();
+
+        match command {
+            "forward" => {
+                forward += number;
+                depth += aim * number;
+            }
+            "down" => aim += number,
+            "up" => aim -= number,
             _ => panic!("unknown command"),
         }
     }
