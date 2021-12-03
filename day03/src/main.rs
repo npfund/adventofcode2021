afde5ece3091098a352f56b8504f4ce2bf75d1a1
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-03 09:15:14 -0700

    Day 3 Part 2 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -46,4 +47,60 @@ fn part1() {
 
 fn to_int(bits: &[u32]) -> u32 {
     bits.iter().fold(0, |acc, &b| acc * 2 + b as u32)
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let lines: Vec<Vec<u32>> = file
+        .lines()
+        .map(|l| {
+            let l = l.unwrap();
+            l.chars().map(|c| c.to_digit(10).unwrap()).collect()
+        })
+        .collect();
+
+    let mut selected = lines.clone();
+    for i in 0..12 {
+        let most = count_bits(&selected, i);
+
+        selected = selected.into_iter().filter(|l| l[i] == most).collect();
+        if selected.len() == 1 {
+            break;
+        }
+    }
+
+    let oxy = to_int(&selected[0]);
+
+    let mut selected = lines.clone();
+    for i in 0..12 {
+        let least = if count_bits(&selected, i) == 0 { 1 } else { 0 };
+
+        selected = selected.into_iter().filter(|l| l[i] == least).collect();
+        if selected.len() == 1 {
+            break;
+        }
+    }
+
+    let co2 = to_int(&selected[0]);
+
+    println!("{}", oxy * co2);
+}
+
+fn count_bits(lines: &Vec<Vec<u32>>, position: usize) -> u32 {
+    let (zero, one) = lines.iter().fold((0, 0), |mut acc, l| {
+        if l[position] == 0 {
+            acc.0 += 1;
+        } else {
+            acc.1 += 1;
+        }
+
+        acc
+    });
+
+    if zero > one {
+        0
+    } else {
+        1
+    }
 }
