97fab88b6df6e72187a88ad4ef0221f3280ae4b4
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-04 09:41:33 -0700

    Day 4 Part 2 complete

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -78,6 +79,88 @@ fn part1() {
                 println!("{}", sum * (choice.parse::<u64>().unwrap()));
 
                 return;
+            }
+        }
+    }
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut lines = file
+        .lines()
+        .map(|l| l.unwrap())
+        .filter(|l| !l.is_empty())
+        .collect::<Vec<_>>();
+
+    let choices = lines
+        .drain(0..1)
+        .collect::<String>()
+        .split(',')
+        .map(|s| s.to_owned())
+        .collect::<Vec<_>>();
+
+    let mut boards = lines
+        .as_slice()
+        .chunks(5)
+        .map(|chunk| {
+            let mut board = HashMap::new();
+            for (y, line) in chunk.iter().enumerate() {
+                for (x, num) in line.split(' ').filter(|c| !c.is_empty()).enumerate() {
+                    board.insert((x, y), (num, false));
+                }
+            }
+
+            board
+        })
+        .collect::<Vec<_>>();
+
+    let mut won = vec![false; boards.len()];
+    for choice in choices {
+        for (board_num, board) in boards.iter_mut().enumerate() {
+            for cell in board.iter_mut() {
+                if cell.1 .0 == choice {
+                    cell.1 .1 = true;
+                }
+            }
+
+            let mut winner = false;
+            for y in 0..5 {
+                let mut row = true;
+                for x in 0..5 {
+                    row = row && board[&(x, y)].1
+                }
+                if row {
+                    winner = true;
+                    break;
+                }
+            }
+
+            for x in 0..5 {
+                let mut col = true;
+                for y in 0..5 {
+                    col = col && board[&(x, y)].1
+                }
+                if col {
+                    winner = true;
+                    break;
+                }
+            }
+
+            if winner {
+                if !won[board_num] && won.iter().filter(|p| !**p).count() == 1 {
+                    let sum = board
+                        .values()
+                        .filter(|v| !v.1)
+                        .map(|v| v.0.parse::<u64>().unwrap())
+                        .sum::<u64>();
+
+                    println!("{}", sum * (choice.parse::<u64>().unwrap()));
+
+                    return;
+                } else {
+                    won[board_num] = true;
+                }
             }
         }
     }
