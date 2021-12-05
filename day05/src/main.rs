cc15b850d27847b1ed1443189fc927104255c71e
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-05 10:19:39 -0700

    Day 5 Part 2 complete

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -40,6 +41,57 @@ fn part1() {
                 *point += 1;
             }
         }
+    }
+
+    let count = points.values().filter(|p| **p > 1).count();
+
+    println!("{}", count);
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut points = HashMap::new();
+    for line in file.lines().map(|l| l.unwrap()) {
+        let (start, end) = line.split_once(" -> ").unwrap();
+        let (x1, y1) = start.split_once(',').unwrap();
+        let (x2, y2) = end.split_once(',').unwrap();
+
+        let x1: i32 = x1.parse().unwrap();
+        let x2: i32 = x2.parse().unwrap();
+        let y1: i32 = y1.parse().unwrap();
+        let y2: i32 = y2.parse().unwrap();
+
+        let mut step = (0, 0);
+        if x1 < x2 {
+            step.0 = 1;
+        } else if x1 > x2 {
+            step.0 = -1;
+        } else {
+            step.0 = 0;
+        }
+
+        if y1 < y2 {
+            step.1 = 1;
+        } else if y1 > y2 {
+            step.1 = -1;
+        } else {
+            step.1 = 0;
+        }
+
+        let mut x = x1;
+        let mut y = y1;
+
+        while x != x2 || y != y2 {
+            let point = points.entry((x, y)).or_insert(0);
+            *point += 1;
+
+            x += step.0;
+            y += step.1;
+        }
+
+        let point = points.entry((x, y)).or_insert(0);
+        *point += 1;
     }
 
     let count = points.values().filter(|p| **p > 1).count();
