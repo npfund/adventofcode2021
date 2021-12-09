9a086610573b0a67423834cdffa3b182d589b200
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-09 09:16:14 -0700

    Day 9 Part 2 complete

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,9 +1,10 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -29,4 +30,59 @@ fn part1() {
     }
 
     println!("{}", sum);
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut map = HashMap::new();
+    for (y, line) in file.lines().map(|l| l.unwrap()).enumerate() {
+        for (x, height) in line.chars().map(|c| c.to_digit(10).unwrap()).enumerate() {
+            map.insert((x as i32, y as i32), height);
+        }
+    }
+
+    let mut basins = Vec::new();
+    for ((x, y), &height) in &map {
+        let up = *map.get(&(*x, y - 1)).unwrap_or(&9);
+        let right = *map.get(&(x + 1, *y)).unwrap_or(&9);
+        let down = *map.get(&(*x, y + 1)).unwrap_or(&9);
+        let left = *map.get(&(x - 1, *y)).unwrap_or(&9);
+
+        if height < up && height < right && height < down && height < left {
+            let mut basin = HashSet::new();
+            flood(&map, &mut basin, *x, *y);
+            basins.push(basin);
+        }
+    }
+
+    basins.sort_by(|a, b| b.len().cmp(&a.len()));
+
+    let total = basins[0].len() * basins[1].len() * basins[2].len();
+
+    println!("{}", total);
+}
+
+fn flood(map: &HashMap<(i32, i32), u32>, basin: &mut HashSet<(i32, i32)>, x: i32, y: i32) {
+    basin.insert((x, y));
+
+    let up = *map.get(&(x, y - 1)).unwrap_or(&9);
+    if up < 9 && !basin.contains(&(x, y - 1)) {
+        flood(map, basin, x, y - 1);
+    }
+
+    let right = *map.get(&(x + 1, y)).unwrap_or(&9);
+    if right < 9 && !basin.contains(&(x + 1, y)) {
+        flood(map, basin, x + 1, y);
+    }
+
+    let down = *map.get(&(x, y + 1)).unwrap_or(&9);
+    if down < 9 && !basin.contains(&(x, y + 1)) {
+        flood(map, basin, x, y + 1);
+    }
+
+    let left = *map.get(&(x - 1, y)).unwrap_or(&9);
+    if left < 9 && !basin.contains(&(x - 1, y)) {
+        flood(map, basin, x - 1, y);
+    }
 }
