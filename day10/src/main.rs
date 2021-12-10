99f30939332cd888b3ebd367191fbe20cfe579a1
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-10 09:23:47 -0700

    Day 10 Part 2 complete

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -3,9 +3,10 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
-#[derive(Eq, PartialEq)]
+#[derive(Debug, Eq, PartialEq)]
 enum Token {
     Paren,
     Square,
@@ -60,4 +61,62 @@ fn part1() {
     }
 
     println!("{}", score);
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut scores = Vec::new();
+    'outer: for line in file.lines().map(|l| l.unwrap()) {
+        let mut stack = Vec::new();
+        for char in line.chars() {
+            match char {
+                '(' => stack.push(Token::Paren),
+                '[' => stack.push(Token::Square),
+                '{' => stack.push(Token::Curly),
+                '<' => stack.push(Token::Angle),
+                ')' => {
+                    let top = stack.pop().unwrap_or(Token::None);
+                    if top != Token::Paren {
+                        continue 'outer;
+                    }
+                }
+                ']' => {
+                    let top = stack.pop().unwrap_or(Token::None);
+                    if top != Token::Square {
+                        continue 'outer;
+                    }
+                }
+                '}' => {
+                    let top = stack.pop().unwrap_or(Token::None);
+                    if top != Token::Curly {
+                        continue 'outer;
+                    }
+                }
+                '>' => {
+                    let top = stack.pop().unwrap_or(Token::None);
+                    if top != Token::Angle {
+                        continue 'outer;
+                    }
+                }
+                _ => panic!("{}", char),
+            }
+        }
+
+        let mut score: i64 = 0;
+        for token in stack.iter().rev() {
+            score *= 5;
+            match token {
+                Token::Paren => score += 1,
+                Token::Square => score += 2,
+                Token::Curly => score += 3,
+                Token::Angle => score += 4,
+                Token::None => panic!("none"),
+            }
+        }
+        scores.push(score);
+    }
+
+    scores.sort();
+    println!("{}", scores[scores.len() / 2]);
 }
