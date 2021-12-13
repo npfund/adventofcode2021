91d6ddcb787b8fd66d0c4cc99a427985f517c54e
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-13 09:51:51 -0700

    Day 11 Part 2 complete

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -49,4 +50,52 @@ fn part1() {
     }
 
     println!("{}", flashes);
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut map = HashMap::new();
+    for (y, line) in file.lines().map(|l| l.unwrap()).enumerate() {
+        for (x, energy) in line.chars().map(|c| c.to_digit(10).unwrap()).enumerate() {
+            map.insert((x as i32, y as i32), energy);
+        }
+    }
+
+    let mut step = 1;
+    loop {
+        let mut flashing: HashSet<(i32, i32)> = HashSet::new();
+        map.values_mut().for_each(|octopus| *octopus += 1);
+
+        loop {
+            let current_flash = flashing.len();
+            for y in 0..10 {
+                for x in 0..10 {
+                    let octopus = map.entry((x, y)).or_default();
+                    if *octopus > 9 && !flashing.contains(&(x, y)) {
+                        flashing.insert((x, y));
+                        for ystep in -1..=1 {
+                            for xstep in -1..=1 {
+                                map.entry((x + xstep, y + ystep)).and_modify(|a| *a += 1);
+                            }
+                        }
+                    }
+                }
+            }
+            if current_flash == flashing.len() {
+                break;
+            }
+        }
+
+        if flashing.len() == map.len() {
+            println!("{}", step);
+            break;
+        }
+
+        for &pos in flashing.iter() {
+            map.entry(pos).and_modify(|oct| *oct = 0);
+        }
+
+        step += 1;
+    }
 }
