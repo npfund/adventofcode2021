1697e3754d808a6aff07875df42b53c1fbd1163c
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-16 14:48:50 -0700

    Day 12 Part 2 complete

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -53,4 +54,70 @@ fn step(cave: &[(String, String)], path: Vec<String>) -> Vec<Vec<String>> {
     }
 
     paths
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let cave = file
+        .lines()
+        .map(|l| {
+            let line = l.unwrap();
+            let l = line.split_once('-').unwrap();
+            (l.0.to_owned(), l.1.to_owned())
+        })
+        .collect::<Vec<_>>();
+
+    let paths = step2(&cave, vec!["start".to_string()]);
+    let complete_paths = paths
+        .iter()
+        .filter(|path| path.last().unwrap() == "end")
+        .count();
+
+    println!("{}", complete_paths);
+}
+
+fn step2(cave: &[(String, String)], path: Vec<String>) -> Vec<Vec<String>> {
+    let mut paths = Vec::new();
+    let position = path.last().unwrap();
+    if position == "end" {
+        return paths;
+    }
+    for s in cave {
+        let destination = if s.0 == *position {
+            &s.1
+        } else if s.1 == *position {
+            &s.0
+        } else {
+            continue;
+        };
+
+        if destination == "start"
+            || (*destination == destination.to_ascii_lowercase()
+                && path.contains(destination)
+                && is_double_poisoned(&path))
+        {
+            continue;
+        }
+
+        let mut new_path = path.to_vec();
+        new_path.push(destination.clone());
+        paths.push(new_path.clone());
+
+        paths.extend(step2(cave, new_path));
+    }
+
+    paths
+}
+
+fn is_double_poisoned(path: &[String]) -> bool {
+    let mut lowercase = path
+        .iter()
+        .filter(|s| **s == s.to_ascii_lowercase())
+        .collect::<Vec<_>>();
+    lowercase.sort();
+    let mut deduped = lowercase.clone();
+    deduped.dedup();
+
+    lowercase != deduped
 }
