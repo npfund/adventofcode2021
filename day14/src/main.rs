db8dccdf28b9bc8fcb82cde2b5e04241e3324cf3
Nick Pfundstein <nickpfundstein@gmail.com>
2021-12-17 12:32:47 -0700

    Day 14 Part 2 complete

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -4,6 +4,7 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -53,4 +54,62 @@ fn part1() {
     let diff = counts.values().max().unwrap() - counts.values().min().unwrap();
 
     println!("{}", diff);
+}
+
+fn part2() {
+    let file = BufReader::new(File::open("input.txt").unwrap());
+
+    let mut counts = HashMap::new();
+    let mut rules = HashMap::new();
+    for line in file.lines().map(|l| l.unwrap()) {
+        if let Some((pattern, insert)) = line.split_once(" -> ") {
+            let pattern = (
+                pattern.chars().nth(0).unwrap(),
+                pattern.chars().nth(1).unwrap(),
+            );
+            let insert = insert.chars().nth(0).unwrap();
+
+            rules.insert(pattern, insert);
+        } else if !line.is_empty() {
+            let template = line.chars().collect::<Vec<_>>();
+            let stream = template
+                .iter()
+                .cloned()
+                .zip(template.iter().skip(1).cloned());
+            for pair in stream {
+                let entry = counts.entry(pair).or_insert(0_u64);
+                *entry += 1;
+            }
+        }
+    }
+
+    for _ in 0..40 {
+        let mut new_counts = counts.clone();
+        for (pair, &insert) in &rules {
+            if let Some(&source) = counts.get(&pair) {
+                let left = new_counts.entry((pair.0, insert)).or_insert(0);
+                *left += source;
+                let right = new_counts.entry((insert, pair.1)).or_insert(0);
+                *right += source;
+                let new_source = new_counts.entry(*pair).or_insert(0);
+                *new_source -= source;
+            }
+        }
+        counts = new_counts;
+    }
+
+    let sums = counts
+        .iter()
+        .fold(HashMap::new(), |mut acc, (pair, &value)| {
+            let left = acc.entry(pair.0).or_insert(0);
+            *left += value;
+            let right = acc.entry(pair.1).or_insert(0);
+            *right += value;
+
+            acc
+        });
+
+    let diff = sums.values().max().unwrap() - sums.values().min().unwrap();
+
+    println!("{}", (diff / 2) + 1);
 }
